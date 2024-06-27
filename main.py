16952f26b0be7468a9611dafe34d8fa7ab5002b2
Aiyin Mi <aynmimoe@gmail.com>
2024-06-28 05:27:25 +0800

    完成匯入腳本

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import txtDict_to_eagleDict
 import jsonDict_to_eagleDict
+import eagle_folder
 import os
+import requests
 
+eagle_api_url = 'http://localhost:41595'
 
 if __name__ == "__main__":
     import sys
 
+    # 取得帶入的參數
     try:
         basePath = sys.argv[1]
     except IndexError as e:
         basePath = "./sample/"
 
+    # 取得Eagle所有資料夾名單
+    eagle_all_folders = eagle_folder.getEagleAllFolder()
+    fileCount = 0
+    errorFilePath = []
+    # 掃描所有檔案
     for root, dirs, files in os.walk(basePath, topdown=False):
         for fileName in files:
-            # print(fileName)
+            fileCount = fileCount+1
             fileNameWithoutExt = fileName.split('.')[0]
             fileExt = fileName.split('.')[-1]
             baseFilePath = os.path.join(root)
             filePath = os.path.join(baseFilePath, fileName)
+            print(str(fileCount)+": "+filePath)
 
             # 若該檔案名稱是meta，代表這個資料夾是一個圖集
             maxNum = 0
@@ -39,19 +49,49 @@ if __name__ == "__main__":
             # 製作Eagle Request Body
             if fileExt == 'txt':
                 txtDict = txtDict_to_eagleDict.readFile_to_txtDict(filePath)
+                if txtDict == {}:
+                    errorFilePath.append(filePath)
+                    continue
                 eagleData = txtDict_to_eagleDict.txtDict_to_eagleDict(txtDict, filePath, maxNum)
+                pUserId = txtDict_to_eagleDict.txtDict_to_puserId(txtDict)
+                pUserName = txtDict_to_eagleDict.txtDict_to_puserName(txtDict)
             elif fileExt == 'json':
                 jsonDict = jsonDict_to_eagleDict.readFile_to_jsonDict(filePath)
+                if jsonDict == {}:
+                    errorFilePath.append(filePath)
+                    continue
                 eagleData = jsonDict_to_eagleDict.jsonDict_to_eagleDict(jsonDict, filePath, maxNum)
+                pUserId = jsonDict_to_eagleDict.jsonDict_to_puserId(jsonDict)
+                pUserName = jsonDict_to_eagleDict.jsonDict_to_puserName(jsonDict)
             else:
                 continue
 
             # 查詢Eagle資料夾id
-            
-            print(eagleData)
+            eagleFolder = eagle_folder.search_folder(eagle_all_folders, pUserName, pUserId)
+            if eagleFolder is not None:
+                eagleFolderId = eagleFolder.get('id')
+            else:
+                authorName = pUserName
+                pid = pUserId
+                eagleFolderId = eagle_folder.createFolder(authorName, pid)
+                eagle_all_folders = eagle_folder.getEagleAllFolder() # 再重新更新一次清單
 
-            
+            folderData = {
+                "folderId": eagleFolderId
+            }
 
+            data = eagleData | folderData
+            # print(data)
+
+            # 新增檔案到Eagle
+            if data is not None:
+                isMultiItem = data.get('items') is not None
+
+                if isMultiItem:
+                    addResp = requests.post(eagle_api_url+'/api/item/addFromPaths', json=data)
+                else:
+                    addResp = requests.post(eagle_api_url+'/api/item/addFromPath', json=data)
+                    
             # filePath = os.path.join(baseFilePath, fileName)
             # fileExt = fileName.split('.')[-1]
 
@@ -68,3 +108,6 @@ if __name__ == "__main__":
 
         # for name in dirs:
         #     print(os.path.join(root, name))
+    
+    print('Error Files: ==================================')        
+    print(errorFilePath)
